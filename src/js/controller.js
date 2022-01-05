9daeb5813e891dffd9cd156204fcc37d186fd4e9
Severyn Kurach <severynson.kurach@gmail.com>
2022-01-06 00:04:25 +0200

    Implementing search result

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,15 +1,10 @@
 import * as model from './model.js'
 import recipeView from './views/recipeView.js';
-
-////////////////
+import searchView from './views/searchView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime'
 import { async } from 'regenerator-runtime';
-
-const recipeContainer = document.querySelector('.recipe');
 // https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const contolRecipes = async () => {
      // 1) Loading recipe;
@@ -30,16 +25,22 @@ const contolRecipes = async () => {
 
 const controlSearchResults = async function () {
   try {
-    await model.loadSearchResults('pizza');
+    // 1) Get search query;
+    const query = searchView.getQuery();
+    if (!query) return;
+    // 2) Load search results;
+    await model.loadSearchResults(query);
+    // 3) Render results;
     console.log(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
 };
-controlSearchResults();
 
 const init = () => {
-  recipeView.addHandlerRender(contolRecipes)
+  recipeView.addHandlerRender(contolRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  
 };
 init();
 
