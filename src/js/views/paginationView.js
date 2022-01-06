6f4c40f9edbc59b148cee7dbc22898007644dec1
Severyn Kurach <severynson.kurach@gmail.com>
2022-01-06 14:08:10 +0200

    implementing pagination - part2 - in progres...+

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -8,6 +8,7 @@ export default new (class PaginationView extends View {
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
         console.log(numPages);
 
+        
         // Page 1, and there are other pages;
         // Page 1, and there are NO oter pages;
         // Last page;
